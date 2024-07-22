cc1244fa9bf31398f64f4d77696200ca5162098c
reyan <reyanabid20@gmail.com>
2024-07-22 23:14:24 +0500

    SSS

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,10 @@
-var best = document.querySelector("#btext");
-var move = document.querySelector(".header_section");
-move.addEventListener("mousemove", function (dets) {
-  gsap.to(move, {
-    x: dets.x,
-    y: dets.Y,
-    duration: 1,
-    ease: "back.out",
-  });
-});
+// var best = document.querySelector("#btext");
+// var move = document.querySelector(".header_section");
+// move.addEventListener("mousemove", function (dets) {
+//   gsap.to(move, {
+//     x: dets.x,
+//     y: dets.Y,
+//     duration: 1,
+//     ease: "back.out",
+//   });
+// });
